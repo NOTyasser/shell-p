fc02c0bf39fcd6bba99adbc17f5b54f89e724b4a
LuntuZAR <luntukazinkungwini@gmail.com>
2023-05-25 13:52:27 -0700

    simple_shell

---
+++ _atoi.c
@@ -0,0 +1,76 @@
+#include "shell.h"
+
+/**
+ * Interactive-Returns true if the shell is in interactive mode.
+ * @info:The structure address.
+ * Returns:1 if in interactive mode, 0 if not.
+ */
+
+int interactive(info_t *info)
+{
+return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delim-It checks if the char is a delimeter.
+ * @c:Is the character that is being checked.
+ * @delim:Is the delimeter string.
+ * Returns:1 if true, 0 if not.
+ */
+
+int is_delim(char c, char *delim)
+{
+while (*delim)
+if (*delim++ == c)
+return (1);
+return (0);
+}
+
+/**
+ * _isalpha - Checks for the alphabetic character.
+ * @c: The character to input.
+ * Returns: 1 if c is an alphabet, 0 if not.
+ */
+
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+return (1);
+else
+return (0);
+}
+
+/**
+ * _atoi - Converts the stringto an integer.
+ * @s -Is the string that will be converted.
+ * Returns:0 if there is no string ,converted numbers if there is'
+ */
+
+int _atoi(char *s)
+{
+int i, sign = 1, flag = 0, output;
+unsigned int result = 0;
+
+for (i = 0; s[i] != '\0' && flag != 2; i++)
+{
+if (s[i] == '-')
+sign *= -1;
+
+if (s[i] >= '0' && s[i] <= '9')
+{
+flag = 1;
+result *= 10;
+result += (s[i] - '0');
+}
+else if (flag == 1)
+flag = 2;
+}
+
+if (sign == -1)
+output = -result;
+else
+output = result;
+
+return (output);
+}
+
---
+++ errors.c
@@ -0,0 +1,86 @@
+#include "shell.h"
+
+/**
+ * _eputs-This prints the input string.
+ * @str:Is the string that will be printed.
+ * Returns:Nothing
+ */
+
+ void _eputs(char *str)
+{
+int i = 0;
+
+if (!str)
+return;
+while (str[i] != '\0')
+{
+_eputchar(str[i]);
+i++;
+}
+}
+
+/**
+ * _eputchar-This writes the character c to the standard error.
+ * @c:The character that will be printed.
+ * Returns:1 if successful,-1 if on error and error
+ * number is appropiately set up.
+ */
+
+int _eputchar(char c)
+{
+static int i;
+static char buf[WRITE_BUF_SIZE];
+
+if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+{
+write(2, buf, i);
+i = 0;
+}
+if (c != BUF_FLUSH)
+buf[i++] = c;
+return (1);
+}
+
+/**
+ * _putfd-It writes the character to the given fd.
+ * @c:Is the character that will be printed.
+ * @fd:The filedescriptor that will be written to.
+ * Returns:1 if successful,-1 if on error and error
+ * number is appropiately set up.
+ */
+
+int _putfd(char c, int fd)
+{
+static int i;
+static char buf[WRITE_BUF_SIZE];
+
+if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+{
+write(fd, buf, i);
+i = 0;
+}
+if (c != BUF_FLUSH)
+buf[i++] = c;
+return (1);
+}
+
+/**
+ * _putsfd -This prints an input string
+ * @str:The string that will be printed
+ * @fd:The filedescriptor that will be written to
+ * Returns: The number of chars put
+ */
+
+int _putsfd(char *str, int fd)
+{
+int i = 0;
+
+if (!str)
+return (0);
+while (*str)
+{
+i += _putfd(*str++, fd);
+}
+return (i);
+}
+
